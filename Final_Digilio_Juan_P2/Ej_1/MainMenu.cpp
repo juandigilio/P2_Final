d15fae83b13895a8cdb52e28adee97a4ea6e3583
Juan Digilio <juanignaciodigilio@gmail.com>
2025-03-07 03:09:59 -0300

    Update MainMenu.cpp

--- Final_Digilio_Juan_P2/Ej_1/MainMenu.cpp
+++ Final_Digilio_Juan_P2/Ej_1/MainMenu.cpp
@@ -33,27 +33,36 @@ void MainMenu::InitSumaTest()
 	center.x = consoleData->GetConsoleCenter().X;
 	center.y = consoleData->GetConsoleCenter().Y;
 
-
 	welcomePos.x = center.x - welcome.length() / 2;
 	welcomePos.y = center.y;
+
 	typePos.x = center.x - type.length() / 2;
 	typePos.y = center.y;
+
 	integerPos.x = center.x - integer.length() / 2;
 	integerPos.y = center.y + 1;
+
 	decimalPos.x = center.x - decimal.length() / 2;
 	decimalPos.y = center.y + 2;
+
 	valuesPos.x = center.x - values.length() / 2;
 	valuesPos.y = center.y;
+
 	values2Pos.x = center.x - values.length() / 2;
 	values2Pos.y = center.y + 1;
+
 	minPos.x = center.x - minText.length() / 2;
 	minPos.y = center.y + 2;
+
 	maxPos.x = center.x - maxText.length() / 2;
 	maxPos.y = center.y + 3;
+
 	roundsQntyPos.x = center.x - roundsQnty.length() / 2;
 	roundsQntyPos.y = center.y;
+
 	roundLengthPos.x = center.x - roundLength.length() / 2;
 	roundLengthPos.y = center.y;
+
 	delayPos.x = center.x - delayText.length() / 2;
 	delayPos.y = center.y;
 

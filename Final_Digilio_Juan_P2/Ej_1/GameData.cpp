c5ad49a16b9c29539951048ebc7a1b5b28335faa
Juan Digilio <juanignaciodigilio@gmail.com>
2025-03-07 00:57:03 -0300

    Added GameData

---
+++ Final_Digilio_Juan_P2/Ej_1/GameData.cpp
@@ -0,0 +1,30 @@
+#include "GameData.h"
+
+
+namespace SumaTestData
+{
+	bool isDecimal = false;
+	string welcome = ">>Opciones de dificultad<<";
+	string type = "Seleccione el tipo de dato: ";
+	string integer = "1- Entero";
+	string decimal = "2- Decimal";
+	string values = "Seleccione el rango de valores: ";
+	string values2 = "Diferencia minima entre min y max (10)";
+	string minText = "Minimo:";
+	string maxText = "Maximo:";
+	string roundsQnty = "Cantidad de rondas:";
+	string roundLength = "Cantidad de numeros por ronda:";
+	string delayText = "Tiempo de respuesta (s):";
+
+	Vector2<int> welcomePos{};
+	Vector2<int> typePos{};
+	Vector2<int> integerPos{};
+	Vector2<int> decimalPos{};
+	Vector2<int> valuesPos{};
+	Vector2<int> values2Pos{};
+	Vector2<int> minPos{};
+	Vector2<int> maxPos{};
+	Vector2<int> roundsQntyPos{};
+	Vector2<int> roundLengthPos{};
+	Vector2<int> delayPos{};
+}
---
+++ Final_Digilio_Juan_P2/Ej_1/GameData.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+
+#include "Vector2.h"
+
+using namespace std;
+
+namespace SumaTestData
+{
+	extern bool isDecimal;
+	extern string welcome;
+	extern string type;
+	extern string integer;
+	extern string decimal;
+	extern string values;
+	extern string values2;
+	extern string minText;
+	extern string maxText;
+	extern string roundsQnty;
+	extern string roundLength;
+	extern string delayText;
+
+	extern Vector2<int> welcomePos;
+	extern Vector2<int> typePos;
+	extern Vector2<int> integerPos;
+	extern Vector2<int> decimalPos;
+	extern Vector2<int> valuesPos;
+	extern Vector2<int> values2Pos;
+	extern Vector2<int> minPos;
+	extern Vector2<int> maxPos;
+	extern Vector2<int> roundsQntyPos;
+	extern Vector2<int> roundLengthPos;
+	extern Vector2<int> delayPos;
+}
